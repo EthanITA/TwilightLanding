7c660f89abbccaed69c981a9cd3a3ab92d661db7
madong <viaimperiale@gmail.com>
2024-03-22 19:21:48 +0100

    add pwa

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/ui', '@nuxt/image'],
+  modules: ['@nuxt/ui', '@nuxt/image', '@vite-pwa/nuxt'],
+  pwa: {},
   devtools: { enabled: true },
   app: {
     head: {
