88d37d4480fbb03eee98956cf4b3c722026a7376
madong <viaimperiale@gmail.com>
2024-04-11 00:52:06 +0200

    feat: more animation

--- components/Hero/Section.vue
+++ components/Hero/Section.vue
@@ -29,9 +29,10 @@
       <div class="text-center">
         <h1
           :class="{
-            'opacity-100': mounted,
+            'text-4xl opacity-100 sm:text-6xl': mounted,
+            'text-xl opacity-0 sm:text-2xl': !mounted,
           }"
-          class="text-4xl font-semibold tracking-tight text-gray-900 opacity-0 transition-all duration-[2500ms] ease-in-out sm:text-6xl"
+          class="font-semibold tracking-tight text-gray-900 transition-all duration-[2500ms] ease-in-out"
         >
           Embrace the Future of
           <span class="flex items-center justify-center gap-2 text-center font-bold text-secondary">
@@ -76,12 +77,21 @@
         </p>
         <div
           :class="{
-            'opacity-100': mounted,
+            'text-lg opacity-100': mounted,
+            'text-sm opacity-0': !mounted,
           }"
-          class="mt-10 flex items-center justify-center gap-x-6 opacity-0 transition-all delay-[5250ms] duration-[3s] ease-in-out"
+          class="mt-10 flex items-center justify-center gap-x-6 transition-all duration-[2s] ease-in-out"
         >
           <a
-            class="before:ease relative overflow-hidden rounded-md bg-secondary px-4 py-3 text-lg font-semibold text-white shadow-2xl transition-all duration-700 before:absolute before:right-0 before:top-0 before:h-12 before:w-6 before:translate-x-12 before:rotate-6 before:bg-white before:opacity-10 before:duration-700 hover:bg-primary/95 hover:shadow-primary hover:before:-translate-x-52 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary"
+            :class="{
+              'rounded-2xl bg-secondary text-white shadow-2xl duration-1000 hover:border-primary hover:bg-primary hover:shadow-primary focus-visible:outline-primary':
+                animationDone,
+              'rounded-md border-secondary text-secondary shadow-md duration-300 hover:border-secondary hover:bg-secondary/95 hover:text-white hover:shadow-secondary focus-visible:outline-secondary':
+                !animationDone,
+              'before:absolute before:right-0 before:top-0 before:h-12 before:w-6 before:translate-x-12 before:rotate-6 before:bg-white before:opacity-10 before:duration-700':
+                animationDone,
+            }"
+            class="before:ease relative overflow-hidden border px-4 py-3 font-semibold transition-all hover:before:-translate-x-52 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
             href="#"
           >
             <span class="relative z-10">Join the waiting list ✨</span>
@@ -122,8 +132,12 @@
 
 <script lang="ts" setup>
 const mounted = ref(false);
+const animationDone = ref(false);
 
 onMounted(() => {
   mounted.value = true;
+  setTimeout(() => {
+    animationDone.value = true;
+  }, 4750);
 });
 </script>
