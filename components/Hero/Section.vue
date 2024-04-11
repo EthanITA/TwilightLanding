1ef8965a8e87990ab6d9840419acbdd97718432c
madong <viaimperiale@gmail.com>
2024-04-11 18:53:02 +0200

    fix(HeroSection): AI Productivity and Twilight selection use respective color

--- components/Hero/Section.vue
+++ components/Hero/Section.vue
@@ -36,7 +36,7 @@
         >
           Embrace the Future of
           <span class="flex items-center justify-center gap-2 text-center font-bold text-secondary">
-            <span> AI Productivity </span>
+            <span class="selection:bg-secondary/40"> AI Productivity </span>
             <svg class="css-1xvso8j" fill="none" height="46" viewBox="0 0 44 46" width="44" xmlns="http://www.w3.org/2000/svg">
               <path
                 d="m3.88 4 22.03 7.49a13.12 13.12 0 0 1 8.36 17.12l5.82 6.43-8.84 8-5.78-6.38A13.11 13.11 0 0 1 9.02 26.02zm0 0 18.05 19.95"
@@ -47,7 +47,7 @@
             </svg>
           </span>
           with
-          <span class="font-extrabold text-primary">Twilight.</span>
+          <span class="select font-extrabold text-primary selection:bg-primary/40">Twilight.</span>
         </h1>
         <p class="mt-6 text-lg leading-8 text-gray-600">
           <span
