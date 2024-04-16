b9849ff070418914e548e7005c71a94634b0a08d
madong <viaimperiale@gmail.com>
2024-04-16 10:39:21 +0200

    feat(CTAButton): add "press" effect on click

--- components/CTA/Button.vue
+++ components/CTA/Button.vue
@@ -7,15 +7,18 @@
     class="mt-10 flex justify-center transition-all duration-[2s]"
   >
     <button
+      ref="button"
       :class="{
-        'rounded-2xl bg-secondary text-base text-white shadow-2xl duration-1000 hover:border-primary hover:bg-primary hover:shadow-primary focus-visible:outline-primary':
+        'rounded-2xl bg-secondary text-base text-white shadow-2xl duration-700 hover:border-primary hover:bg-primary hover:shadow-primary focus-visible:outline-primary':
           animationDone,
         'rounded-md text-sm text-secondary duration-300 hover:border-secondary hover:bg-secondary/95 hover:text-white focus-visible:outline-secondary':
           !animationDone,
         'before:ease before:absolute before:right-0 before:top-0 before:h-12 before:w-6 before:translate-x-12 before:rotate-6 before:bg-white before:opacity-10 before:duration-700 hover:before:-translate-x-52':
           animationDone,
+        '!translate-y-0.5 !shadow-none !duration-150': clicked,
       }"
       class="relative overflow-hidden border border-secondary px-4 py-3 font-semibold transition-all focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
+      @mousedown="CtaClick"
     >
       <span class="relative z-10">Join the Waitlist ✨</span>
     </button>
@@ -28,6 +31,15 @@ const animationDone = ref(false);
 const props = defineProps<{
   animationWait?: number;
 }>();
+const clicked = ref(false);
+const CtaClick = () => {
+  if (clicked.value) return;
+  clicked.value = true;
+  setTimeout(() => {
+    clicked.value = false;
+  }, 150);
+};
+
 onMounted(() => {
   mounted.value = true;
   setTimeout(() => {
