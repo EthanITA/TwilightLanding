f30e6bc42edf07377273cae03da8aeb59cc7cc0b
madong <viaimperiale@gmail.com>
2024-04-22 22:26:51 +0200

    fix(CTAButton): compact label

--- components/CTA/Button.vue
+++ components/CTA/Button.vue
@@ -10,7 +10,7 @@
       class="before:ease relative overflow-hidden rounded-2xl border border-secondary bg-secondary px-4 py-3 text-base font-semibold text-white shadow-2xl transition-all duration-700 before:absolute before:right-0 before:top-0 before:h-12 before:w-6 before:translate-x-12 before:rotate-6 before:bg-white before:opacity-10 before:duration-700 hover:border-primary hover:bg-primary hover:shadow-primary hover:before:-translate-x-52 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary"
       @mousedown="CtaClick"
     >
-      <span class="relative z-10">Join the Waitlist ✨</span>
+      <span class="relative z-10">Join Waitlist ✨</span>
     </button>
   </div>
 </template>
