bdce9f467d0c16c6fa5e03ee7b93d0df46cdd313
madong <viaimperiale@gmail.com>
2024-04-22 22:24:54 +0200

    fix(Heading): improve message

--- components/Hero/Heading.vue
+++ components/Hero/Heading.vue
@@ -1,15 +1,8 @@
-<script lang="ts" setup></script>
-
 <template>
-  <h1 class="text-4xl font-semibold tracking-tight text-gray-900 sm:text-6xl" data-aos="zoom-in">
-    Embrace the Future of
-    <span class="flex items-center justify-center gap-2 text-center font-bold text-secondary">
-      <span class="selection:bg-secondary/40"> Productivity </span>
-      <IconPen />
-    </span>
-    with
+  <h1 class="flex flex-col items-center text-4xl font-semibold tracking-tight sm:text-6xl" data-aos="zoom-in">
     <span class="select font-extrabold text-primary selection:bg-primary/40">Twilight.</span>
+    <span>The AI-driven</span>
+    <span class="text-center font-bold text-secondary selection:bg-secondary/40"> Productivity </span>
+    <span class="w-fit"> platform </span>
   </h1>
 </template>
-
-<style scoped></style>
