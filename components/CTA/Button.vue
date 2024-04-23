94a329508df2001fd39223e4a1d4a03d9a751b7d
madong <viaimperiale@gmail.com>
2024-04-23 16:50:23 +0200

    fix(CTAButton): use active instead of logical trigger

--- components/CTA/Button.vue
+++ components/CTA/Button.vue
@@ -4,11 +4,8 @@
     <button
       id="join-waitlist"
       ref="button"
-      :class="{
-        '!translate-y-0.5 !shadow-none !duration-150': clicked,
-      }"
-      class="before:ease relative overflow-hidden rounded-2xl border border-secondary bg-secondary px-4 py-3 text-base font-semibold text-white shadow-2xl transition-all duration-700 before:absolute before:right-0 before:top-0 before:h-12 before:w-6 before:translate-x-12 before:rotate-6 before:bg-white before:opacity-10 before:duration-700 hover:border-primary hover:bg-primary hover:shadow-primary hover:before:-translate-x-52 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary"
-      @mousedown="CtaClick"
+      class="before:ease relative overflow-hidden rounded-2xl border border-secondary bg-secondary px-4 py-3 text-base font-semibold text-white shadow-2xl transition-all duration-700 before:absolute before:right-0 before:top-0 before:h-12 before:w-6 before:translate-x-12 before:rotate-6 before:bg-white before:opacity-10 before:duration-700 hover:border-primary hover:bg-primary hover:shadow-primary hover:before:-translate-x-52 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary active:translate-y-0.5 active:shadow-none active:duration-150"
+      @mouseup="() => (open = true)"
     >
       <span class="relative z-10">Join Waitlist ✨</span>
     </button>
@@ -16,15 +13,6 @@
 </template>
 <script lang="ts" setup>
 const open = ref(false);
-const clicked = ref(false);
-const CtaClick = () => {
-  if (clicked.value) return;
-  clicked.value = true;
-  setTimeout(() => {
-    clicked.value = false;
-    open.value = true;
-  }, 220);
-};
 </script>
 
 <style scoped></style>
