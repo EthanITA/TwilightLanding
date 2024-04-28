376b5b320a12dfcc2b4acca7340bafca2f6e21fa
madong <viaimperiale@gmail.com>
2024-04-28 19:57:14 +0200

    feat: add cookie consent popup

--- app.vue
+++ app.vue
@@ -1,4 +1,11 @@
 <template>
   <NuxtPage />
+  <div
+    v-auto-animate="{
+      duration: 1000,
+    }"
+  >
+    <CookiePopup data-aos="fade-in" data-aos-delay="1500" />
+  </div>
 </template>
 <script lang="ts" setup></script>
---
+++ components/Button.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+import { computed } from 'vue';
+
+const props = defineProps<{
+  color?: 'primary' | 'none';
+}>();
+
+const color = computed(() =>
+  props.color === 'primary'
+    ? 'bg-primary text-gray-50 shadow-sm hover:shadow-md focus-visible:bg-primary focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 active:shadow-none active:duration-150 active:translate-y-0 hover:-translate-y-0.5'
+    : 'bg-none text-gray-500 hover:bg-gray-100 focus-visible:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2',
+);
+</script>
+
+<template>
+  <button :class="color" class="rounded-2xl px-4 py-2 transition-all duration-200">
+    <slot />
+  </button>
+</template>
+
+<style scoped></style>
