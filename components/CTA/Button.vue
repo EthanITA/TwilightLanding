e87e5b235e7023559b30ade6caf531a1d122faa1
madong <viaimperiale@gmail.com>
2024-04-28 10:21:31 +0200

    feat(CTAButton): click triggers ctaClick event

--- components/CTA/Button.vue
+++ components/CTA/Button.vue
@@ -9,7 +9,7 @@
         'rounded-xl px-3 py-2 text-sm': !big,
       }"
       class="before:ease relative overflow-hidden border border-secondary bg-secondary font-semibold text-white shadow-2xl transition-all duration-1000 before:absolute before:right-0 before:top-0 before:h-12 before:w-6 before:translate-x-12 before:rotate-6 before:bg-white before:opacity-10 before:duration-700 hover:border-primary hover:bg-primary hover:shadow-primary hover:before:-translate-x-52 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary active:translate-y-0.5 active:shadow-none active:duration-150"
-      @mouseup="() => (open = true)"
+      @mouseup="CtaClick"
     >
       <span class="relative z-10">Sign up ✨</span>
     </button>
@@ -20,6 +20,10 @@ const open = ref(false);
 defineProps<{
   big?: boolean;
 }>();
+const CtaClick = () => {
+  open.value = true;
+  dataLayer?.push({ event: 'ctaClick' });
+};
 </script>
 
 <style scoped></style>
